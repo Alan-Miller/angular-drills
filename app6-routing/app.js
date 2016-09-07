4196a1ef16e8fcdc13cddec987e02fe88d57ed8d
Alan-Miller <alan.ashman@gmail.com>
2016-09-07 13:20:19 -0600

    early stages

--- app6-routing/app.js
+++ app6-routing/app.js
@@ -1,7 +1,23 @@
 angular.module('routingApp', ['ui.router'])
-.config(function() {
+.config(function($stateProvider, $urlRouterProvier) {
 
+  $urlRouterProvier.otherwise('/');
 
+  $stateProvider
+  .state('home', {
+    url: '/',
+    controller: 'homeController',
+    templateUrl: 'home/home.html'
+  })
+  .state('details', {
+    url: '/details',
+    controller: 'detailsController',
+    templateUrl: 'details/details.html'
+  }).state('signup', {
+    url: '/signup',
+    controller: 'signupController',
+    templateUrl: 'signup/signup.html'
+  });
 
 
 });
