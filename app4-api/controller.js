0c85b9e00e543598e7212355e543682cdc7625eb
Alan-Miller <alan.ashman@gmail.com>
2017-05-02 14:25:20 -0600

    changes

--- app4-api/controller.js
+++ app4-api/controller.js
@@ -2,6 +2,13 @@ angular.module('app4').controller('Ctrl4', function($scope, service4) {
 
   $scope.test = "YES, THERE YOU ARE"
 
-  $scope.chars = service3.chars;
+  $scope.getChars = function() {
+
+    service4.getChars().then(function() {
+      
+    });
+    return chars.data;
+  };
+  $scope.getChars();
 
 });
--- app4-api/service.js
+++ app4-api/service.js
@@ -1,7 +1,10 @@
 angular.module('app4').service('service4', function($http) {
 
   this.getChars = function() {
-
+    return $http({
+      method: 'GET',
+      url: 'https://swapi.co/api/people/'
+    });
   };
 
 });
